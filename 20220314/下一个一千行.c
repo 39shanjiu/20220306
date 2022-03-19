48d9b17db2f748171a88a54ad3221c31e945bd53
39shanjiu <1918251321@qq.com>
2022-03-19 22:15:30 +0800

    继续

--- 20220314/下一个一千行.c
+++ 20220314/下一个一千行.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+﻿//#include <stdio.h>
 //void test()
 //{
 //	printf("hehe");
@@ -279,9 +279,285 @@
 //	//该函数有两个参数，一个是int，另一个是函数指针。
 //	return 0;52222222222221656541
 //}232333333333333222222222222316465
-typedef unsigned int uint;
+//typedef unsigned int uint;
+//
+//int main()
+//{
+//	char* My_strcpy(char* dest, const char* src);
+//	char* (*pr)(char*, const char*);
+//	char* (*pfarr[4])(char*, const char*);
+//	int k = 0;
+//	int i;
+//	return 0;
+//}
+//void meau()
+//{
+//	    printf("#######################\n");
+//		printf("##1.Add         2.Sub##\n");
+//		printf("##3.Div         3.Mul##\n");
+//		printf("##      0. exit      ##\n");
+//		printf("#######################\n");
+//}
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	int x = 0;
+//	int y = 0;
+//	//pfarr 函数指针数组  也被称为转移表。
+//	int (*pfarr[])(int, int) = {0,Add,Sub,Div,Mul};
+//	do
+//	{
+//		meau();
+//		printf("请选择：");
+//		scanf_s("%d", &input);
+//		if (input > 0 && input < 5)
+//		{
+//			printf("请输入两个操作数：");
+//			scanf_s("%d%d", &x, &y);
+//			int ret = pfarr[input]( x, y);
+//			printf("%d\n", pfarr[input](x, y));
+//			//break;
+//		}
+//		else if (input == 0)
+//		{
+//			printf("退出\n");
+//		}
+//		else
+//		{
+//			printf("输入错误\n");
+//		} 
+//	} while (input);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int(*p)[10] = &arr;//数组指针
+//	int (*pfArr[4])(int, int);//函数指针数组
+//	int (*(*ppfArr)[4])(int, int) = &pfArr;//
+//	//ppfArr先于*结合，所以是指针该指针再与[4]结合
+//	//成为一个数组指针，剩下的int (*)(int, int)是个函数指针类型
+//	//所以ppfArr是一个数组指针，有四个元素，每个元素是一个函数指针。
+//	return 0;
+//}
+//void qsort(float arr[], int sz)
+//{
+//
+//}
+//int main()
+//{
+//	float arr[] = {0,1.3,2.7,3,6,5.2,-4.6,7};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr,sz);
+//	for()
+//	return 0;
+//}#include <math.h>
+//#include <stdlib.h>
+//void* p = &a;
+////void*类型可以接受任意类型的地址
+////但是不能解引用，还有++--；
+//float cmp_float(const void* e1,const void* e2)
+//{
+//	return (float*)e1 - (float*)e2;
+//}
+//int main()
+//{
+//	float arr[] = { 20.3,2.5,6.5,8.69,3.68,7.36,8.21 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr, sz, arr[0], cmp_float);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%f ", arr[i]);
+//	}
+//	return 0;
+//}
+/*#include <stdio.h>
+#include <string.h>
+struct stu
+{
+	char name[20];
+	int age;
+};
+void swap(char* s1, char* s2,int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		int ret = *s1;
+		*s1 = *s2;
+		*s2 = ret;		
+	}
+}
+void My_qsort(void* ss,int sz,int width,int (*cmp)(void*, void*))
+{
+	int i = 0;
+	int k = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		for (k = 0; k < sz - i - 1; k++)
+		{
+			if (cmp((char*)ss + k * width, (char*)ss + (k + 1) * width) > 0)
+			{
+				swap((char*)ss + k * width, (char*)ss + (k + 1) * width,width);
+			}
+		}
+	}
+}
+int cmp_str_name(const void* e1,const void* e2)
+{
+	return strcmp(((struct stu*)e1)->name, ((struct stu*)e2)->name);
+}
+
 int main()
 {
-	int i;
+	struct stu s[3] = { {"zhangsan",20},{"lisi",30},{"wangwu",10} };
+	int arr[] = { 9,8,7,6,5,4,3,2,1 };
+	int sz = sizeof(s) / sizeof(s[0]);
+	My_qsort(s, sz, sizeof(s[0]), cmp_str_name);
+	return 0;
+}*/
+#include <stdio.h>
+#include <string.h>//strcmp
+void Swap(char* s1, char* s2,int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		int tmp = *s1;
+		*s1 = *s2;
+		*s2 = tmp;
+		*s1++;
+		*s2++;
+	}
+}
+//开始实现升序
+//此处用冒泡排序
+void My_qsort(void* base, int sz, int width, int (*cmp)(void*, void*))
+{
+	//传的是不同的函数名，但是接收的都是cmp，所以函数名都是cmp。
+	//函数传过来的是地址，所以可以用函数指针接收
+	//可不可以直接用一个函数接收？就像数组一样？
+	int k = 0;
+	int i = 0;
+	//趟数
+	for (i = 0; i < sz - 1; i++)//sz个元素，只需要sz-1趟
+	{
+		//每趟要比较的次数
+		//每趟除去自身，所以-1
+		//每过一趟，最后一个元素就以确定（-i）
+		for (k = 0; k < sz - 1 - i; k++)
+		{
+			//开始比较两个元素,此处用cmp函数实现
+			//用宽度来实现第一个元素与下一个元素
+			//char* 的类型字节长度为一
+			//将元素地址强制转换为char*类型
+			//元素是几个字节还是几个字节
+			//用元素地址改变元素
+			if (cmp((char*)base + k * width, (char*)base + (k + 1) * width) > 0)
+			{
+				//如果为真，交换这两个元素,
+				//由于数组类型不确定，所以还是要依靠宽度来实现交换
+				Swap((char*)base + k * width, (char*)base + (k + 1) * width, width);
+			}
+
+		}
+	}
+}
+struct stu
+{
+	char name[10];
+	int age;
+};
+//比较两个数据的大小，前者大输出大于0的数
+//相等输出0，小于输出小于0的数
+// 对于字符串，有一个专门的函数来比较大小。
+int cmp_stu_name(const void* e1,const void* e2)
+{
+	//强行转换为构造体指针类型,构造体名要用括号，再用->指向构造体的元素。
+	//strcmp与cmp一样，输出方式相同。
+	return strcmp(((struct stu*)e1)->name, ((struct stu*)e2)->name);
+}
+int cmp_stu_age(const void* e1, const void* e2)
+{
+	return (((struct stu*)e1)->age - ((struct stu*)e2)->age);
+}
+int cmp_char_arr(const void* e1, const void* e2)
+{
+	return strcmp( e1,  e2);//这里为什么可以实现啊
+}
+int cmp_int_arr(const void* e1, const void* e2)
+{
+	return (*(int *)e1 - *(int *)e2);
+}
+float cmp_float_arr(const void* e1, const void* e2)
+{
+	return *(float*)e1 - *(float*)e2;
+}
+void test1()
+{
+	//实现任意类型数组升序包括构造体
+	struct stu s[3] = { {"zhangsan",20},{"lisi",30},{"wangwu",10} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	//My_qsort(void*，int，int，cmp）
+	//该函数实现升序功能
+	//最后一个参数为实现比较功能的函数
+	//第一个参数为想要升序的数组，第二个为数组长度
+	//第三个为数组宽度（每个元素所占字节长度）
+	//由于要实现不同类型的数组升序，各个类型的数组一个元素所占字节的长度不一样
+	//所以要依靠宽度来完成两个元素的交换。
+	My_qsort(s, sz, sizeof(s[0]), cmp_stu_name);
+} 
+void test2()
+{
+	struct stu s[3] = { {"zhangsan",20},{"lisi",30},{"wangwu",10} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	My_qsort(s, sz, sizeof(s[0]), cmp_stu_age);
+}
+void test3()
+{
+	char arr[] = "acbfehg";
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	My_qsort(arr, sz, sizeof(arr[0]), cmp_char_arr);
+}
+void test4()
+{
+	int arr[] = {2,5,69,8,78,3,1,5,4,8};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	My_qsort(arr, sz, sizeof(arr[0]), cmp_int_arr);
+}
+//根据浮点数的在内存中的存储与整数不同
+//还无法实现
+void test5()
+{
+	float arr[] = { 2.0,6.9,5.8,5.2,99.6,7.6,8.5 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	My_qsort(arr, sz, sizeof(arr[0]), cmp_float_arr);
+}
+//先实现一个特定数组的升序，再完善回调函数。 
+int main()
+{
+	//test1();//实现构造体name的升序函数
+	//test2();//实现构造体age的升序函数
+	//test3();//实现char类型数组升序函数
+	test4();//实现int类型数组升序
+	//test5();//实现float类型数组升序
 	return 0;
 }
